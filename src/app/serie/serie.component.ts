29bac4404c58763368d02f879a3ecdb9f488266c
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-04-13 15:19:07 -0500

    Average seasons hecho

--- src/app/serie/serie.component.ts
+++ src/app/serie/serie.component.ts
@@ -12,12 +12,22 @@ export class SerieComponent implements OnInit {
 
 
   constructor(private serieService: SerieService) { }
-  series!: Array<Serie>;
+  series: Array<Serie> = [];
+  avrg_seasons: number = 0;
 
   getSeries()
   {
     this.serieService.getSeries().subscribe(series => {
       this.series = series;
+
+      let totalSeasons: number = 0;
+      let totalShows: number = series.length;
+      console.log(series);
+      console.log(totalShows);
+      series.forEach((serie)=> totalSeasons+= serie.seasons);
+  
+      let resp: number = totalSeasons/totalShows;
+      this.avrg_seasons = resp;
     });
   }
 
