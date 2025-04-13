a1bd9c7ed7adb384dd3a70dc2957f0f9e13e0f91
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-04-13 12:23:16 -0500

    modulo serie creado

---
+++ src/app/serie/serie.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-serie',
+  templateUrl: './serie.component.html',
+  standalone: false,
+  styleUrls: ['./serie.component.css']
+})
+export class SerieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
